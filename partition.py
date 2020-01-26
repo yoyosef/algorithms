31a077d68b1f459611b432d144bbc22b74e3988f
Yosef Leibman <yosefleibman@gmail.com>
2020-01-25 19:32:40 -0500

    added annotations to randomized_select

--- partition.py
+++ partition.py
@@ -27,14 +27,22 @@ def randomized_quicksort(array, p, r):
 def randomized_select(array, p, r, i):
     if p == r:
         return array[p]
-
+    # q is the partition point returned by randomized_partition
     q = randomized_partition(array, p, r)
+    # A[q] is the kth smallest element from p to r
     k = q - p + 1
+
+    # Found the ith smallest element
     if i == k:
         return array[q]
-    elif (i < k):
-        return randomized_select(array, p, q- 1, i)
+    # The ith element is in the left partition
+    elif i < k:
+        # exclude q because it has been already checked
+        # need to find the ith of the left partition
+        return randomized_select(array, p, q - 1, i)
     else:
+        # since we've 'removed' the left partition, we want to find the (i-k)th element
+        # of the right partition
         return randomized_select(array, q + 1, r, i - k)
 
 if __name__ == '__main__':
